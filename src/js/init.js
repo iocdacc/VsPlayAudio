205919d1509d3f4f70ea59f05b0ed60f40ae6265
iocdacc <iocdacc@gmail.com>
2020-02-04 18:13:04 +0800

    修改软件信息

--- src/js/init.js
+++ src/js/init.js
@@ -1,6 +1,6 @@
 export default function(){
     console.log(
-        '\n%c VsPlayAudio:%c https://github.com/iocdacc \n',
+        '\n%c vsPlayAudio:%c https://blog.iocdacc.com \n',
         'color: #fff;background-image: linear-gradient(90deg, rgb(47, 172, 178) 0%, rgb(45, 190, 96) 100%);padding:5px 1px;',
         'color: #fff;background-image: linear-gradient(90deg, rgb(45, 190, 96) 0%, rgb(255, 255, 255) 100%);padding:5px 0;width: 200px;display: inline-block;'
     );
